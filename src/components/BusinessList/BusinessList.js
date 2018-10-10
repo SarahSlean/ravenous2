c4b76ffecbdfc7905d24b10b089da26572e80f03
SarahSlean <yoda4@hotmail.fr>
2018-10-10 10:51:41 +0200

    update files App.js Business.js BusinessList.js after the taks 1-6 and 9 I have to solve tasks 7 and 8

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,14 +7,11 @@ class BusinessList extends React.Component {
   render() {
   	return (
   		<div className="BusinessList">
-  		  <Business />
-  		  <Business />
-  		  <Business />
-  		  <Business />
-  		  <Business />
-  		  <Business />
-  		</div>
-  		    )
+      {
+        this.props.businesses.map(business());
+      }
+      </div>
+      )
   }
 }
 
